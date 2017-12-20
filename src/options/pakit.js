087fa2d9531dc27e992965042d8b9bbd48b87523
Miguel Castillo <manchagnu@gmail.com>
2017-12-20 15:55:39 -0500

    adding logic to continue to load .bundlerrc files to allow for a transition period.

--- src/options/pakit.js
+++ src/options/pakit.js
@@ -6,16 +6,18 @@ function buildPakitOptions() {
   var config = require(path.join(__dirname, "../../", ".pakit"));
 
   if (glob.sync(path.join(process.cwd(), ".bundlerrc*(.js|.json)")).length) {
-    process.stderr.write(`${chalk.cyan.bold("pakit:")} ${chalk.yellow.bold(".bitbundlerc is deprecated")}. Please rename to ${chalk.bold(".pakit")} instead.\n`);
+    process.stderr.write(`${chalk.cyan.bold("pakit:")} ${chalk.yellow.bold(".bundlerrc is deprecated")}. Please rename to ${chalk.bold(".pakit")} instead. We will continue to load .bundlerrc files for now.\n`);
+    Object.assign(config, require(path.join(process.cwd(), ".bundlerrc")));
   }
-
-  try {
-    Object.assign(config, require(path.join(process.cwd(), ".pakit")));
-  }
-  catch(ex) {
-    if (ex.code !== "MODULE_NOT_FOUND") {
-      throw ex;
+  else {
+    try {
+      Object.assign(config, require(path.join(process.cwd(), ".pakit")));
     }
+    catch(ex) {
+      if (ex.code !== "MODULE_NOT_FOUND") {
+        throw ex;
+      }
+    }  
   }
 
   return config;
