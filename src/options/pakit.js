816983fc39605c0ff34370cfb75e2b4dc8844a67
Miguel Castillo <manchagnu@gmail.com>
2017-12-20 15:47:34 -0500

    Adding a warning when bitbundlerrc files are found for awareness.

--- src/options/pakit.js
+++ src/options/pakit.js
@@ -1,7 +1,13 @@
-var path = require("path");
+const path = require("path");
+const glob = require("glob");
+const chalk = require("chalk");
 
 function buildPakitOptions() {
   var config = require(path.join(__dirname, "../../", ".pakit"));
+
+  if (glob.sync(path.join(process.cwd(), ".bundlerrc*(.js|.json)")).length) {
+    process.stderr.write(`${chalk.cyan.bold("pakit:")} ${chalk.yellow.bold(".bitbundlerc is deprecated")}. Please rename to ${chalk.bold(".pakit")} instead.\n`);
+  }
 
   try {
     Object.assign(config, require(path.join(process.cwd(), ".pakit")));
